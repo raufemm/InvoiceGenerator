6b24b25d405c8cf1ce6a26b60aabee9acbfd83f4
Raufe <raufe.m@gmail.com>
2023-01-27 16:45:11 -0300

    Add project files.

---
+++ InvoiceGenerator.App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InvoiceGenerator.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
